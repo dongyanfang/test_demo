121d8a2b611bdf36d20fdf1b30e09a9c04ddf495
dongyf <1471404820@qq.com>
2017-08-12 10:06:28 +0800

    创建一个延迟加载的服务

--- src/app/test44/test44.component.ts
+++ src/app/test44/test44.component.ts
@@ -1,4 +1,8 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import {TestService} from '../service/test.service';
+import {Hero} from './data-model';
+import 'rxjs/add/operator/finally';
 
 @Component({
   selector: 'app-test44',
@@ -6,11 +10,29 @@ import {Component, EventEmitter, OnInit, Output} from '@angular/core';
   styleUrls: ['./test44.component.css']
 })
 export class Test44Component implements OnInit {
-  public demo=['demo1','demo2','demo3'];
+  public demo= ['demo1', 'demo2', 'demo3'];
   public demoIndex = 2; // current demo
-  constructor() { }
+  public isLoading= true;
+  heroes: Observable<Hero[]>;
+  selectedHero: Hero;
+  constructor(private TestService: TestService) { }
 
   ngOnInit() {
+  }
+  getHeroes() {
+    this.isLoading = true;
+    this.heroes =  this.TestService.getHeroes().finally(() => console.log('aaa'));
+      // Observable.of("xxxx").delay(3000).subscribe(data => console.log(data));
+    // this.heroes = this.TestService.getHeroes()
+    this.heroes.subscribe(v => console.log(v));
+
+    this.selectedHero = undefined;
+  }
+  selectDemo(num) {
+    console.log(num);
+    this.demoIndex = num;
+    this.getHeroes();
+
   }
 
 }
--- src/app/test44/test44.module.ts
+++ src/app/test44/test44.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { Test44RoutingModule } from './test44-routing.module';
-import {Test44Component} from "./test44.component";
-
+import {Test44Component} from './test44.component';
+import { TestService } from '../service/test.service';
+import { Demo1Component } from './demo1/demo1.component'; //
 @NgModule({
   imports: [
     CommonModule,
     Test44RoutingModule
   ],
-  declarations: [Test44Component]
+  declarations: [Test44Component, Demo1Component],
+  providers: [ TestService ],
 })
 export class Test44Module { }
